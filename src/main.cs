c14e48f7ca1265cdff30d598af34b28ee8ab3c37
muhammadsaad <mu7ammadsa3dd@gmail.com>
2025-05-09 16:22:57 +0300

    fix: include $ sign on the prompt

--- src/main.cs
+++ src/main.cs
@@ -1,12 +1,12 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
 
 // Wait for user input
 while (true)
 {
+    Console.Write("$ ");
+
     var command = Console.ReadLine();
 
     System.Console.WriteLine($"{command}: command not found");
