8912d33237172e115787feca61498d9ae4b8a25c
muhammadsaad <mu7ammadsa3dd@gmail.com>
2025-05-26 17:42:06 +0300

    print multiple matches after second tab press

--- src/InputReader.cs
+++ src/InputReader.cs
@@ -6,6 +6,7 @@ public static class InputReader
 {
     public static string Read()
     {
+        ConsoleKey? lastKey = null;
         var keyBuffer = new StringBuilder();
 
         while (true)
@@ -14,6 +15,7 @@ public static class InputReader
 
             if (key.Key == ConsoleKey.Enter)
             {
+                lastKey = null;
                 Console.WriteLine();
                 break;
             }
@@ -24,29 +26,48 @@ public static class InputReader
                 {
                     var prefix = keyBuffer.ToString();
 
-                    var matches = Startup.Commands.GetWords(prefix);
-                    var match = matches.FirstOrDefault();
+                    var matches = Startup.Commands.GetWords(prefix).ToList();
                     
-                    if (string.IsNullOrEmpty(match))
+                    if (matches.Count == 0)
                     {
                         Console.Beep();
                     }
+                    else if(matches.Count > 1)
+                    {
+                        if(lastKey != ConsoleKey.Tab) Console.Beep();
+                        else
+                        {
+                            Console.WriteLine();
+                            foreach (var match in matches.Order())
+                            {
+                                Console.Write($"{match}  ");
+                            }
+                            
+                            Console.WriteLine();
+                            Console.Write($"$ {keyBuffer}");
+                        }
+                    }
                     else
                     {
+                        var match = matches[0];
+                        
                         Console.Write(match[prefix.Length..] + " ");
 
                         keyBuffer.Append(match[prefix.Length..] + " ");
                     }
 
+                    lastKey = ConsoleKey.Tab;
                     break;
                 }
                 case ConsoleKey.Backspace when keyBuffer.Length > 0:
                     Console.Write("\b \b");
-
+                    lastKey = null;
                     keyBuffer.Length--;
                     break;
                 default:
                 {
+                    lastKey = null;
+                        
                     if (!char.IsControl(key.KeyChar))
                     {
                         Console.Write(key.KeyChar);
